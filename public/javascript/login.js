a0b77c0f371efa286f22665e6115cb440944b43b
Franko Grancarić <grancaric.franko@gmail.com>
2019-05-02 13:25:09 +0200

    updated login form

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,14 @@
-function handleChangeLabel() {
-    let label = document.querySelector('.loginForm__input');
-    console.log(label);
+function handleChangeLabel(input) {
+
+    $(input).children('.loginForm__input').blur(function () {
+        if ($(input).children('.loginForm__input').val().length !== 0) {
+            $(input).children('.loginForm__label').css({ 'color': 'green', 'font-size': '15px', 'top': '0px' });
+        } else {
+            $(input).children('.loginForm__label').css({ 'color': 'black', 'font-size': '18px', 'top': '25px' });
+        }
+    });
+
+    $(input).children('.loginForm__input').focus(function () {
+        $(input).children('.loginForm__label').css({ 'color': 'green', 'font-size': '15px', 'top': '0px' });
+    });
 }
