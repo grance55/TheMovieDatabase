57f9f54ca82325b865b28efaed23c6ef17c9c74c
Marko Mihelj <marko.mihelj@infinum.hr>
2019-05-03 21:41:35 +0200

    move movie fetch logic to controller

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const router = express.Router();
 
-const env = require('../config/env');
 const checkAuth = require('../middleware/checkAuth');
-const moviesList = require('../config/movieList');
-const Movie = require('../models/movies');
+const MoviesController = require('../controllers/movies');
 
 router.get('/', checkAuth, async (req, res) => {
   const page = req.query.page || 1;
-
-  const movieRes = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${env.API_KEY}&language=en-US&page=${page}`);
-  const moviesData = await movieRes.json();
-  const movieArray = moviesData.results.map((item) => new Movie(item));
+  const movieArray = await MoviesController.fetchHomeMovies(page);
 
   res.render('index', {
     title: 'The Movie Database',
--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,7 @@
 const express = require('express');
-// const fetch = require('node-fetch');
 const router = express.Router();
 
 const checkAuth = require('../middleware/checkAuth');
-// const env = require('../config/env');
-// const moviesList = require('../config/movieList');
-// const Movie = require('../models/movies');
 
 router.get('/', checkAuth, async (req, res) => {
   res.render('profile', {
