58afaa6d3b4c31904dc4cac58b71fdabe9c2b700
Franko Grancarić <grancaric.franko@gmail.com>
2019-05-10 00:05:33 +0200

    Updated single movie UI, and profile.ejs

--- models/movies.js
+++ models/movies.js
@@ -5,7 +5,7 @@ class Movie {
     this.title = movie.title;
     this.year = movie.release_date.slice(0, 4);
     this.plot = movie.overview;
-    this.cover = movie.backdrop_path;
+    this.cover = `http://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
     this.vote = movie.vote_average;
     this.duration = movie.runtime;
     this.rate = movie.vote_average;
---
+++ public/javascript/movie.js
@@ -0,0 +1,7 @@
+let main = $('.movie'),
+    navbar = $('.navbar'),
+    heightMain = $('.wrapper').height() - navbar.height();
+
+console.log($(window).height());
+console.log(navbar.height());
+
